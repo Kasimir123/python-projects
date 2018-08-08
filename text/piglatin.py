767494a292bd97d97240a62b06ce2b20d8d699d8
kasimir123 <kasimirkhschulz@gmail.com>
2018-08-08 00:24:39 +0000

    Added english to pig latin translator, will need to update double consonant list as well as figure out how to make that words that start with w translate correctly

--- text/piglatin.py
+++ text/piglatin.py
@@ -1,5 +1,6 @@
 original = input("What would you like to translate into pig latin?")
 text = original.lower()
+double_consonants = "ch th sp sl gr wh "
 
 def eng_to_pigltn(text):
     words = text.split()
@@ -20,5 +21,26 @@ def eng_to_pigltn(text):
         
     
     return pig_latin
+    
+def pigltn_to_eng(text):
+    words = text.split()
+    english = ""
+    
+    for word in words:
+        if len(word) == 1:
+            english = english + word + " "
+        elif word[-3:] == "way" and word[0] in "aeiou":
+            word = word[0:-3]
+            english = english + word + " "
+        elif word[-3] not in "aeiou"  and word[0] in "aeiou" and word[-4:-2] not in double_consonants:
+            word = word[-3] + word[0:-3]
+            english = english + word + " "
+        elif word[-4] not in "aeiou" and word[-3] not in "aeiou" and word[-4:-2] in double_consonants:
+            word = word[-4] + word[-3] + word[0:-4]
+            english = english + word + " "
+            
+    return english
         
 print(eng_to_pigltn(text))
+
+print(pigltn_to_eng(eng_to_pigltn(text)))
